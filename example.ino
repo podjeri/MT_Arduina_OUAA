7cff01e70546839220c19589e6a0ab0ebc9d6855
chamomilatea <129452221+chamomilatea@users.noreply.github.com>
2023-04-04 00:32:45 -0500

    Update example.ino

--- example.ino
+++ example.ino
@@ -1,2 +1,11 @@
 // hello world!
 println (); 
+Planet planet;
+Planet planet2;
+Planet moon;
+
+void setup() {
+  size(1200, 1200);
+  planet = new Planet(200, 2, 80, 0);
+  planet2 = new Planet(200, 2, 40, 180);
+}
